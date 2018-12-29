4ec172b8e8fa9a30717e262ec632496c0146ec86
Patrick Browne <pt.browne@gmail.com>
2018-12-29 16:51:30 +0100

    feat: Graceful fail if no network connection

--- src/sources/git.js
+++ src/sources/git.js
@@ -9,14 +9,26 @@ module.exports = async (dir, repo, branch, root) => {
   const docsDir = syspath.join(repoDir, root)
 
   try {
-    await simpleGit()
-      .silent(true)
-      .clone(repo, repoDir, { '--depth': 1 })
+    const existence = fs.existsSync(repoDir)
+    if (existence) {
+      try {
+        await simpleGit(repoDir)
+          .silent(true)
+          .fetch()
+      } catch (e) {
+        console.warn(`Could not fetch ${repoDir}`)
+      }
+    } else {
+      await simpleGit()
+        .silent(true)
+        .clone(repo, repoDir, { '--depth': 1 })
+    }
 
     await simpleGit(repoDir)
       .silent(true)
       .checkout(branch)
   } catch (err) {
+    console.log(err)
     throw new Error(`Could not find branch in ${repo}: ${branch}`)
   }
 
