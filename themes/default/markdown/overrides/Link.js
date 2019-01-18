a4e26694f0584a2c179581893d9cbf8e555c2348
Patrick Browne <pt.browne@gmail.com>
2019-01-18 14:33:12 +0100

    feat: Support anchor without href used by jsdoc

--- themes/default/markdown/overrides/Link.js
+++ themes/default/markdown/overrides/Link.js
@@ -5,7 +5,11 @@ export default function (props) {
   const { href, children, ...rest } = props
   const isExternal = /^https?:\/\//.test(href)
 
-  return isExternal
-    ? <a {...props} target="_blank">{children}</a>
-    : <Link {...rest} to={href}>{children}</Link>
+  if (isExternal) {
+    return <a {...props} target="_blank">{children}</a>
+  } else if (!href) {
+    return <a {...props}>{children}</a>
+  } else {
+    return <Link {...rest} to={href}>{children}</Link>
+  }
 }
