f35d3a2bd4abde656ad40bb5f0c16d9dced6c34c
Patrick Browne <pt.browne@gmail.com>
2019-07-28 15:27:53 +0200

    feat: Throw meaningful error when walking soruces

--- src/core/source.js
+++ src/core/source.js
@@ -4,36 +4,41 @@ const { dirTree } = require('./filesystem')
 // const { log, styles } = require('../utils/emit')
 
 async function walkSource (tempDir, currentDir, data) {
-  const {
-    source,
-    source_type: type = 'local',
-    source_root: root = 'docs',
-    source_branch: branch = 'master',
-  } = data
+  try {
+    const {
+      source,
+      source_type: type,
+      source_root: root,
+      source_branch: branch,
+    } = data
 
-  // log(`Fetching ${type} source: ${styles.note(source)}`)
+    // log(`Fetching ${type} source: ${styles.note(source)}`)
 
-  let resultDir
-  switch (type) {
-    case 'git': {
-      resultDir = await sourceGit(tempDir, source, branch, root)
-      break
+    let resultDir
+    switch (type) {
+      case 'git': {
+        resultDir = await sourceGit(tempDir, source, branch, root)
+        break
+      }
+
+      case 'http':
+      case 'https':
+      case 'static': {
+        throw new Error('HTTP sources are not supported yet.')
+      }
+
+      case 'local':
+      default: {
+        resultDir = await sourceLocal(source, currentDir)
+        break
+      }
     }
 
-    case 'http':
-    case 'https':
-    case 'static': {
-      throw new Error('HTTP sources are not supported yet.')
-    }
-
-    case 'local':
-    default: {
-      resultDir = await sourceLocal(source, currentDir)
-      break
-    }
+    return dirTree(resultDir)
+  } catch (e) {
+    console.error('Error while walking source', source)
+    throw e
   }
-
-  return dirTree(resultDir)
 }
 
 module.exports = {
