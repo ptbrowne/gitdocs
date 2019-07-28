0cb69fbb6d234b7c158944e318e19c1eab09180e
Patrick Browne <pt.browne@gmail.com>
2019-07-28 16:14:31 +0200

    refactor: Extract rx to test for external links

--- themes/default/markdown/overrides/Link.js
+++ themes/default/markdown/overrides/Link.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const absoluteLinkRx = /^https?:\/\//
+
 export default function (props) {
   const { href, children, ...rest } = props
-  const isExternal = /^https?:\/\//.test(href)
+  const isExternal = absoluteLinkRx.test(href)
 
   if (isExternal) {
     return <a {...props} target="_blank">{children}</a>
